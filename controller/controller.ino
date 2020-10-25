353f2b288717ad5f400a917b64d3816087af1469
Morita-Ntyb <64995616+Morita-Ntyb@users.noreply.github.com>
2020-10-25 18:14:33 +0900

    Update controller.ino

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+const int potentiometer0 = A0;
+const int potentiometer1 = A1;
+const int potentiometer2 = A2;
+
+int potentiometer0_Value = 0;
+int potentiometer1_Value = 0;
+int potentiometer2_Value = 0;
+
+void setup() 
+{  
+  
+  Serial.begin(9600);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  
+  potentiometer0_Value = analogRead(potentiometer0);
+  potentiometer1_Value = analogRead(potentiometer1);
+  potentiometer2_Value = analogRead(potentiometer2);
 
+  Serial.print(potentiometer0_Value);
+  Serial.print(",");
+  Serial.print(potentiometer0_Value);
+  Serial.print(",");
+  Serial.println(potentiometer0_Value);
+
+  delay(2); //ADC待機のため
+  
 }
