133d967571cf407685963dff7d8b3d753f9fafe8
Morita-Ntyb <64995616+Morita-Ntyb@users.noreply.github.com>
2020-11-09 16:03:55 +0900

    Update exhibit.ino

--- exhibit/exhibit.ino
+++ exhibit/exhibit.ino
@@ -8,7 +8,7 @@
 PCA9685 pwm = PCA9685(0x40);//オブジェクト, アドレス指定
 
 #define SERVOMIN 205 //1周期4096(ステップ)に対するサーボのパルス幅(ステップ)
-#define SERVOMAX 983 //
+#define SERVOMAX 983
 #define PWMFREQ 50  //PWM周期
 
 LSM9DS1 imu;  //LSM9DS1のオブジェクトを作成
@@ -18,6 +18,9 @@ LSM9DS1 imu;  //LSM9DS1のオブジェクトを作成
 void printAccel();
 void shiftArray();
 double average(const double*, int);
+
+int serial_to_value(char*, int*);
+
 
 void startUpSequence();
 
@@ -61,11 +64,17 @@ static double k2 = 195.3476;
 static double k3 = 0;
 static double l1 = 398.1524;
 
-char serial_string[25]; //シリアル通信 バッファ
-int serial_int[4];
+char serial_string[16]; //シリアル通信 バッファ
+int serial_int[4] = {0};
+char gommy;
+int j;
 
 void setup()
 {
+  Serial.begin(9600);
+  Serial.println("start setup...");
+
+  
   pwm.begin();                   //初期設定
   pwm.setPWMFreq(PWMFREQ);       //PWM周期を設定
 
@@ -75,44 +84,45 @@ void setup()
   {
     while (1);
   }
-
+/*
   while(1)
   {
-    serial_string[0] = Serial.read();
-    if(serial_string[0] == '\0')
+    gommy = Serial.read();
+    Serial.print(gommy);
+    if(gommy == 'F')
     {
       break;
     }
   }
+  */
   
   //startUpSequence();
 
+  Serial.println("end setup...");
 }
 
 void loop()
 {
-
+  if (Serial.available() > 16)
+  { 
+    for(j=0; j < 16; j++)
+    {
+      serial_string[j] = Serial.read();
+    }
+  }
   
   if ( imu.accelAvailable() )
   {
     imu.readAccel(); //IMUの値を更新
   }
 
-  if (Serial.available() >= 22)
-  { 
-    for(int i=0; i<=22; i++)
-    {
-      serial_string[i] = Serial.read();
-  
-      if (serial_string[i] == '\0')
-      {
-        break;        
-      }
 
-    }
+  serial_to_value(serial_string, serial_int);
+
+  for (j= 0; j< 3; j++)
+  {
+    serial_int[j] = map(serial_int[j], 0, 1024, 205, 985);
   }
-
-  //ここにchar型配列をint型に変換する関数を挿入 serial_string >> serial_int
 
   shiftArray(); //配列を一つ後ろにずらす
 
@@ -135,29 +145,36 @@ void loop()
                    + h1*zFAccel*zFAccel*zFAccel + h2*zFAccel*zFAccel + h3*zFAccel + i1;
   double servoDeg4 = j1*xFAccel*xFAccel*xFAccel + j2*xFAccel*xFAccel + j3*xFAccel
                    + k1*zFAccel*zFAccel*zFAccel + k2*zFAccel*zFAccel + k3*zFAccel + l1;
-                   
-  pwm.setPWM(0, 0, (int)((servoDeg1+0.5)/2.0));
-  pwm.setPWM(2, 0, (int)((servoDeg2+0.5)/2.0));
-  pwm.setPWM(13, 0, (int)((servoDeg3+0.5)/2.0));
-  pwm.setPWM(15, 0, (int)((servoDeg4+0.5)/2.0));
-  
 
+    pwm.setPWM(0, 0, (int)((servoDeg1+0.5)/2.0));
+    pwm.setPWM(2, 0, (int)((servoDeg2+0.5)/2.0));
+    pwm.setPWM(13, 0, (int)((servoDeg3+0.5)/2.0));
+    pwm.setPWM(15, 0, (int)((servoDeg4+0.5)/2.0));
+
+  /*
+  {
+    pwm.setPWM(0, 0, serial_int[0]);
+    pwm.setPWM(2, 0, serial_int[1]);
+    pwm.setPWM(13, 0, serial_int[2]);
+    pwm.setPWM(15, 0, serial_int[0]);
+  }
+  */
 }
 
 void shiftArray()
 {
-  
-  
+
+
   for(int i=filterPoint -1 ; i > 0; --i)
   {
     xAccelBuffer[i] = xAccelBuffer[ i-1 ];
   }
-  
+
   for(int i=filterPoint -1 ; i > 0; --i)
   {
     yAccelBuffer[i] = yAccelBuffer[ i-1 ];
   }
-  
+
   for(int i=filterPoint -1 ; i > 0; --i)
   {
     zAccelBuffer[i] = zAccelBuffer[ i-1 ];
@@ -175,7 +192,19 @@ double average(const double* array, int size)
     }
     return (result / (double)size ) ;
 }
-    
+
+int serial_to_value(char* serial_string, int* target_array)
+{
+  int i=0;
+  target_array[0] = ((int)serial_string[i]- 48 ) +( (int)serial_string[i+ 1]- 48 )* 10+ ((int)serial_string[i+ 2]- 48) * 100 + ((int)serial_string[i +3]- 48) * 1000 ;
+  i += 4 ;  
+  target_array[1] =( (int)serial_string[i]- 48 ) + ((int)serial_string[i+ 1]- 48 )* 10+ ((int)serial_string[i+ 2]- 48) * 100 + ((int)serial_string[i +3] - 48)* 1000 ;
+  i += 4 ;  
+  target_array[2] = ((int)serial_string[i]- 48 ) + ((int)serial_string[i+ 1] - 48) * 10+ ((int)serial_string[i+ 2]- 48 )* 100 + ((int)serial_string[i +3]- 48) * 1000 ;
+
+  target_array[3] = serial_string[12] - 48;
+}
+
 
 /*
 void startUpSequence()
@@ -201,7 +230,7 @@ void startUpSequence()
 
     pwm.setPWM(2, 0, servoDeg4); //左 あげ
     pwm.setPWM(2, 0, servoDeg4); //右 あげ
-
+    
     delay(1100);
     
 
